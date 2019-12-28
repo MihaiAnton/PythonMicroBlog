f39c7afbace49e6cae092b1bc335cad4fec2f153
Mihai Anton <mihai_anton98@yahoo.com>
2019-12-28 14:04:56 +0200

    Fixed errors

--- app/translate.py
+++ app/translate.py
@@ -1,13 +1,11 @@
 import json
 
-import nltk
+
 import requests
 from flask import current_app
 from flask_babel import _
 
 
-STOPWORDS_DICT = {lang: set(nltk.corpus.stopwords.words(lang))
-                  for lang in nltk.corpus.stopwords.fileids()}
 
 def translate(text, source_language, dest_language):
     if 'MS_TRANSLATOR_KEY' not in current_app.config or \
@@ -23,9 +21,3 @@ def translate(text, source_language, dest_language):
         return _('Error: the translation service failed.')
     return json.loads(r.content.decode('utf-8-sig'))
 
-def guess_language(text):
-    words = set(nltk.wordpunct_tokenize(text.lower()))
-    lang = max(((lang, len(words & stopwords))
-                for lang, stopwords in STOPWORDS_DICT.items()),
-               key=lambda x: x[1])[0]
-    return lang[:2]
